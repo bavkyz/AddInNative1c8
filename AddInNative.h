24b6ae185cf59055710511586c9004a4f205270b
bavkyz <bavkyz@mail.ru>
2019-11-26 12:21:46 +0700

    перый комит

--- AddInNative.h
+++ AddInNative.h
@@ -10,94 +10,101 @@
 class CAddInNative : public IComponentBase
 {
 public:
-    enum Props
-    {
-        ePropIsEnabled = 0,
-        ePropIsTimerPresent,
-        ePropLast      // Always last
-    };
+	enum Props
+	{
+		ePropPortTypeIsTCP = 0,
+		ePropLocalIP,
+		ePropLocalPort,
+		ePropStatus,
+		ePropLogFile,
+		ePropLast      // Always last
 
-    enum Methods
-    {
-        eMethEnable = 0,
-        eMethDisable,
-        eMethShowInStatusLine,
-        eMethStartTimer,
-        eMethStopTimer,
-        eMethLoadPicture,
-        eMethShowMsgBox,
+	};
+
+	enum Methods
+	{
+		eMethShowInStatusLine,
+		eMethExternalEvent,
+		eMethLoadPicture,
+		eMethShowMsgBox,
+		eMethOpenPort,
+		eMethClosePort,
+		eMethNotifyPort,
+		eMethPause,
 		eLoopback,
-        eMethLast      // Always last
-    };
+		eMethLast      // Always last
+	};
 
-    CAddInNative(void);
-    virtual ~CAddInNative();
-    // IInitDoneBase
-    virtual bool ADDIN_API Init(void*);
-    virtual bool ADDIN_API setMemManager(void* mem);
-    virtual long ADDIN_API GetInfo();
-    virtual void ADDIN_API Done();
-    // ILanguageExtenderBase
-    virtual bool ADDIN_API RegisterExtensionAs(WCHAR_T**);
-    virtual long ADDIN_API GetNProps();
-    virtual long ADDIN_API FindProp(const WCHAR_T* wsPropName);
-    virtual const WCHAR_T* ADDIN_API GetPropName(long lPropNum, long lPropAlias);
-    virtual bool ADDIN_API GetPropVal(const long lPropNum, tVariant* pvarPropVal);
-    virtual bool ADDIN_API SetPropVal(const long lPropNum, tVariant* varPropVal);
-    virtual bool ADDIN_API IsPropReadable(const long lPropNum);
-    virtual bool ADDIN_API IsPropWritable(const long lPropNum);
-    virtual long ADDIN_API GetNMethods();
-    virtual long ADDIN_API FindMethod(const WCHAR_T* wsMethodName);
-    virtual const WCHAR_T* ADDIN_API GetMethodName(const long lMethodNum, 
-                            const long lMethodAlias);
-    virtual long ADDIN_API GetNParams(const long lMethodNum);
-    virtual bool ADDIN_API GetParamDefValue(const long lMethodNum, const long lParamNum,
-                            tVariant *pvarParamDefValue);   
-    virtual bool ADDIN_API HasRetVal(const long lMethodNum);
-    virtual bool ADDIN_API CallAsProc(const long lMethodNum,
-                    tVariant* paParams, const long lSizeArray);
-    virtual bool ADDIN_API CallAsFunc(const long lMethodNum,
-                tVariant* pvarRetValue, tVariant* paParams, const long lSizeArray);
-    // LocaleBase
-    virtual void ADDIN_API SetLocale(const WCHAR_T* loc);
-    
+	CAddInNative(void);
+	virtual ~CAddInNative();
+	// IInitDoneBase
+	virtual bool ADDIN_API Init(void*);
+	virtual bool ADDIN_API setMemManager(void* mem);
+	virtual long ADDIN_API GetInfo();
+	virtual void ADDIN_API Done();
+	// ILanguageExtenderBase
+	virtual bool ADDIN_API RegisterExtensionAs(WCHAR_T**);
+	virtual long ADDIN_API GetNProps();
+	virtual long ADDIN_API FindProp(const WCHAR_T* wsPropName);
+	virtual const WCHAR_T* ADDIN_API GetPropName(long lPropNum, long lPropAlias);
+	virtual bool ADDIN_API GetPropVal(const long lPropNum, tVariant* pvarPropVal);
+	virtual bool ADDIN_API SetPropVal(const long lPropNum, tVariant* varPropVal);
+	virtual bool ADDIN_API IsPropReadable(const long lPropNum);
+	virtual bool ADDIN_API IsPropWritable(const long lPropNum);
+	virtual long ADDIN_API GetNMethods();
+	virtual long ADDIN_API FindMethod(const WCHAR_T* wsMethodName);
+	virtual const WCHAR_T* ADDIN_API GetMethodName(const long lMethodNum,
+		const long lMethodAlias);
+	virtual long ADDIN_API GetNParams(const long lMethodNum);
+	virtual bool ADDIN_API GetParamDefValue(const long lMethodNum, const long lParamNum,
+		tVariant* pvarParamDefValue);
+	virtual bool ADDIN_API HasRetVal(const long lMethodNum);
+	virtual bool ADDIN_API CallAsProc(const long lMethodNum,
+		tVariant* paParams, const long lSizeArray);
+	virtual bool ADDIN_API CallAsFunc(const long lMethodNum,
+		tVariant* pvarRetValue, tVariant* paParams, const long lSizeArray);
+	virtual bool ADDIN_API MyExternalEvent();
+	virtual bool ADDIN_API wstring_to_p(std::wstring str, tVariant* val);
+	virtual char* ADDIN_API wchar_to_char(const wchar_t* Source, UINT CodePage);
+	// LocaleBase
+	virtual void ADDIN_API SetLocale(const WCHAR_T* loc);
+
 private:
-    long findName(const wchar_t* names[], const wchar_t* name, const uint32_t size) const;
-    void addError(uint32_t wcode, const wchar_t* source, 
-                    const wchar_t* descriptor, long code);
-    // Attributes
-    IAddInDefBase      *m_iConnect;
-    IMemoryManager     *m_iMemory;
+	long findName(const wchar_t* names[], const wchar_t* name, const uint32_t size) const;
+	void addError(uint32_t wcode, const wchar_t* source,
+		const wchar_t* descriptor, long code);
+	// Attributes
+	IAddInDefBase* m_iConnect;
+	IMemoryManager* m_iMemory;
 
-    bool                m_boolEnabled;
-    uint32_t            m_uiTimer;
-#if !defined( __linux__ ) && !defined(__APPLE__)
-    HANDLE              m_hTimer;
-    HANDLE              m_hTimerQueue;
-#endif //__linux__
+	bool                m_boolPortTypeIsTCP;
+	int32_t             m_i4LocalPort;
+	std::wstring        m_strStatus, m_strLogFile, m_LocalIP;
+	SOCKET              m_socket;
+	const int32_t       m_i4MaxSizeOfPacket = 1024;
 };
 
 class WcharWrapper
 {
 public:
 #ifdef LINUX_OR_MACOS
-    WcharWrapper(const WCHAR_T* str);
+	WcharWrapper(const WCHAR_T* str);
 #endif
-    WcharWrapper(const wchar_t* str);
-    ~WcharWrapper();
+	WcharWrapper(const wchar_t* str);
+	~WcharWrapper();
 #ifdef LINUX_OR_MACOS
-    operator const WCHAR_T*(){ return m_str_WCHAR; }
-    operator WCHAR_T*(){ return m_str_WCHAR; }
+	operator const WCHAR_T* () { return m_str_WCHAR; }
+	operator WCHAR_T* () { return m_str_WCHAR; }
 #endif
-    operator const wchar_t*(){ return m_str_wchar; }
-    operator wchar_t*(){ return m_str_wchar; }
+	operator const wchar_t* () { return m_str_wchar; }
+	operator wchar_t* () { return m_str_wchar; }
 private:
-    WcharWrapper& operator = (const WcharWrapper& other) { return *this; }
-    WcharWrapper(const WcharWrapper& other) { }
+	WcharWrapper& operator = (const WcharWrapper& other) { return *this; }
+	WcharWrapper(const WcharWrapper& other) { }
 private:
 #ifdef LINUX_OR_MACOS
-    WCHAR_T* m_str_WCHAR;
+	WCHAR_T* m_str_WCHAR;
 #endif
-    wchar_t* m_str_wchar;
+	wchar_t* m_str_wchar;
 };
 #endif //__ADDINNATIVE_H__
